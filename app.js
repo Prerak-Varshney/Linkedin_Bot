d87a5467445cba349cf7fca7d41d0aca56ce360d
Aryan-eats <e22cseu1302@bennett.edu.in>
2025-02-21 19:01:00 +0530

    Updated

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-import loginToLinkedIn from './lib/login.js';
-import searchJobs from './lib/searchJobs.js';
+import linkedinBot from './lib/LinkedIn.js';
 import { GLOBAL_WAIT_TIME_FOR_SELECTORS } from './config/env.js';
 
 //middlewares
 puppeteer.use(StealthPlugin());
-(
-  async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
 
-    await page.setViewport({ width: 1000, height: 730});
-    
-    page.setDefaultNavigationTimeout(GLOBAL_WAIT_TIME_FOR_SELECTORS);
+const app = async() => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-    try {
-      await page.setUserAgent(
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
-      );
+  await page.setViewport({ width: 1000, height: 730});
+  
+  page.setDefaultNavigationTimeout(GLOBAL_WAIT_TIME_FOR_SELECTORS);
 
-      // Login to LinkedIn
-      await loginToLinkedIn(page);
+  try {
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+    );
 
-      // Search for jobs
-      await searchJobs(page);
+    await linkedinBot(page);
 
-    } catch (error) {
-      console.error('An error occurred:', error);
+  } catch (error) {
+    console.error('An error occurred:', error);
 
-    } finally {
-      await browser.close();
-    }
+  } finally {
+    await browser.close();
   }
-)();
+}
+
+app();
